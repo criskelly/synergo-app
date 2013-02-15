5d00b3f56c790ef744cb4d706abea18cfba5d92e
Cris Kelly <cris.kelly@gmail.com>
2013-02-15 11:42:34 -0800

    removed some files

--- create.php
+++
@@ -1,5 +0,0 @@
-<div class="header">
-	<div class="home-title">
-		<h1>Create Your Day</h1>
-	</div>	
-</div>
--- footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer">
-	<div class="footer-contents">
-    	<a href="today.php"><div class="today"><p>TODAY</p></div></a>
-        <a href="favs.php"><div class="favs"><p>FAVS</p></div></a>
-        <a href="#"><div class="more"><p>MORE</p></div></a>
-    </div>
-</div>
